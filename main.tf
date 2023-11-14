31350590deb1847a51ff98a7fc882b437af2dd96
vishal <vishal.warhade@prorigosoftware.com>
2023-11-14 11:15:07 +0530

    providers.tf file added

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-
-  required_version = ">= 1.1.0"
-}
-
-provider "azurerm" {
-  features {}
-}
-
 module "resource_group" {
   source = "./modules/resource_group"
   resource_group_name  = var.resource_group_name
